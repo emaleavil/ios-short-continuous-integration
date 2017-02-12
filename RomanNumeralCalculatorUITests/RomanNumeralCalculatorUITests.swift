5f65bdc24fb26977d38c748b1d374c37cb816f26
Emanuel <emaleavil@gmail.com>
2017-02-12 23:35:59 +0100

    add ui test for one plus one

--- RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
+++ RomanNumeralCalculatorUITests/RomanNumeralCalculatorUITests.swift
@@ -19,7 +19,8 @@ class RomanNumeralCalculatorUITests: XCTestCase {
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
+        
+        XCUIDevice.shared().orientation = .portrait
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
@@ -28,9 +29,20 @@ class RomanNumeralCalculatorUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAddOnePlusOne() {
+        
+        let app = XCUIApplication()
+        let iButton = app.buttons["I"]
+        let calculatedLabel = app.staticTexts["calculated_value"]
+        
+        iButton.tap()
+        XCTAssertEqual(calculatedLabel.label, "I")
+        app.buttons["+"].tap()
+        iButton.tap()
+        XCTAssertEqual(calculatedLabel.label, "II")
+        
+        
+        
     }
     
 }
